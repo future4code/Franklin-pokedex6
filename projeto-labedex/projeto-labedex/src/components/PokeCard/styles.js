13af166786a8d7e3a4a46c45a26a5834eefc2d84
Davi Emmanuel <davi.emannuellre@gmail.com>
2022-07-05 12:12:08 -0300

    página inicail do projeto labedex

---
+++ projeto-labedex/projeto-labedex/src/components/PokeCard/styles.js
@@ -0,0 +1,11 @@
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import styled from 'styled-components';
+
+export const CardStyled = styled(Card)`
+   margin : 10px 5px;
+`
+
+export const CardImage = styled(CardMedia)`
+    min-height: max-content;
+`
